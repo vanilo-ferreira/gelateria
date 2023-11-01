3033f64859a6b7ec79a0ae60faf24568d19b3802
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2023-11-01 12:20:59 -0300

    feat: Estilizar o componenete Topo

--- src/componentes/Topo/index.js
+++ src/componentes/Topo/index.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import './style.css';
+
 export default function Topo() {
   return (
-    <header>
-      <div>
-        <figure>
-          <img src="assets/logo.png" alt="Logo da Gelatreia" title="Logo da Gelateria" />
-        </figure>
-        
+    <header className="topo topo-position">
+      <div className="limitar-secao topo">
+        <img className="imagem-topo" src="assets/logo.png" alt="Logo da Gelatreia" title="Logo da Gelateria" />
+
         <nav>
-          <Link href="#home">Home</Link>
-          <Link href="#sabores">Sabores</Link>
-          <Link href="#sobre">Sobre</Link>
+          <Link href="#home" className="link-topo">Home</Link>
+          <Link href="#sabores" className="link-topo">Sabores</Link>
+          <Link href="#sobre" className="link-topo">Sobre</Link>
         </nav>
       </div>
     </header>
